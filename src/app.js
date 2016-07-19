f46ef71909a2555412076edad7233ebd55a3bbdf
Kent C. Dodds <kent@doddsfamily.us>
2016-07-19 01:40:55 -0600

    add react graph

--- src/app.js
+++ src/app.js
@@ -1,31 +1,28 @@
 import 'todomvc-common/base.css'
 import 'todomvc-app-css/index.css'
+import './app.css'
 
-import View from './view'
 import {$on} from './helpers'
-import Controller from './controller'
-import Model from './model'
-import Store from './store'
-import Template from './template'
+import {updateTodo} from './todo'
+import toggleGraph from './graph'
 
-/**
- * Sets up a brand new Todo list.
- *
- * @param {string} name The name of your new to do list.
- */
-function Todo(name) {
-  this.storage = new Store(name)
-  this.model = new Model(this.storage)
-  this.template = new Template()
-  this.view = new View(this.template)
-  this.controller = new Controller(this.model, this.view)
-}
+$on(window, 'load', onLoad)
+$on(window, 'hashchange', updateTodo)
 
 // this is exported for Hot Module Replacement
-export function setView() { // eslint-disable-line import/prefer-default-export
-  var todo = new Todo('todos-vanillajs')
-  todo.controller.setView(document.location.hash)
+export function onLoad() { // eslint-disable-line import/prefer-default-export
+  updateTodo()
+  const toggleGraphButton = document.querySelector('.toggle-graph')
+  $on(
+    toggleGraphButton,
+    'click',
+    () => {
+      const active = toggleGraph()
+      if (active) {
+        toggleGraphButton.classList.add('active')
+      } else {
+        toggleGraphButton.classList.remove('active')
+      }
+    },
+  )
 }
-
-$on(window, 'load', setView)
-$on(window, 'hashchange', setView)
--- src/hmr.js
+++ src/hmr.js
@@ -1,11 +1,11 @@
 /* eslint no-console:0 */
 // special thanks to Eric Clemmons: https://github.com/ericclemmons/webpack-hot-server-example
-import {setView} from './app'
+import {onLoad} from './app'
 
 if (module.hot) {
   module.hot.accept('./app', function() {
     console.log('🔁  HMR Reloading `./app`...')
-    setView() // reinitialize the view
+    onLoad() // reinitialize the view
   })
   console.info('✅  HMR Enabled!')
 } else {
